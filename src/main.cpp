6d19083227c0016646ba61b67d2f250a31b244be
Andrei Odintsov <forestryks1@gmail.com>
2019-05-24 01:06:23 +0300

    Added output example in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -33,28 +33,17 @@ int main() {
 
     context->run();
 
-//    std::unique_ptr<libsbox::execution_context> test_context(new libsbox::execution_context());
-//    std::unique_ptr<libsbox::execution_target> solution_target(new libsbox::execution_target());
-//    std::unique_ptr<libsbox::execution_target> interactor_target(new libsbox::execution_target());
-//    std::unique_ptr<libsbox::execution_context> interaction_context(new libsbox::execution_context());
-//    interaction_context->register_target(solution_target.get());
-//    interaction_context->register_target(interactor_target.get());
-//
-//    interaction_context->create_pipe(solution_target->stdout, interactor_target->stdin, 0);
-//    interaction_context->create_pipe(interactor_target->stdout, solution_target->stdin, 0);
-//
-//    solution_target->set_time_limit(1000);
-//    solution_target->set_memory_limit(256 * 1024);
-//    interaction_context->set_wall_time_limit(2000);
-//
-//    interactor_target->add_path_rule("test.in", "test.in", libsbox::R_COPY_TO_BOX);
-//    interactor_target->add_path_rule("test.out", "test.out", libsbox::R_COPY_FROM_BOX);
-//
-//    interaction_context->run();
-//
-//    std::unique_ptr<libsbox::execution_metadata> solution_metadata(solution_target->get_execution_metadata());
-//
-//    std::cout << "exit-code:" << solution_metadata->exit_code << "\n";
-//    std::cout << "time-usage:" << solution_metadata->time_usage << "\n";
-//    std::cout << "memory-usage:" << solution_metadata->memory_usage << "\n";
+    using namespace std;
+    cout << "exited: " << solution_target->exited << endl;
+    cout << "exit_code: " << solution_target->exit_code << endl;
+    cout << "signaled: " << solution_target->signaled << endl;
+    cout << "term_signal: " << solution_target->term_signal << endl;
+    cout << "time_limit_exceeded: " << solution_target->time_limit_exceeded << endl;
+    cout << "wall_time_limit_exceeded: " << solution_target->wall_time_limit_exceeded << endl;
+    cout << "oom_killed: " << solution_target->oom_killed << endl;
+    cout << "time_usage: " << solution_target->time_usage << endl;
+    cout << "time_usage_sys: " << solution_target->time_usage_sys << endl;
+    cout << "time_usage_user: " << solution_target->time_usage_user << endl;
+    cout << "wall_time_usage: " << solution_target->wall_time_usage << endl;
+    cout << "memory_usage: " << solution_target->memory_usage << endl;
 }
